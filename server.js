6afd8218a1c1b15329bb5a77623ba0829174b286
Dmitrii <golubev.rocket@gmail.com>
2019-04-09 11:55:55 +0300

    Version 1.0.10

--- server.js
+++ server.js
@@ -36,10 +36,9 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
 
-const port = 5000;
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
