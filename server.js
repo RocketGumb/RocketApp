8fa22b43640fa23417a6a5bda5617129b4207626
Dmitrii <golubev.rocket@gmail.com>
2019-04-09 10:41:31 +0300

    Version 1.0.5

--- server.js
+++ server.js
@@ -33,11 +33,11 @@ app.use('/api/auth', auth);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('client/build'))
+	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const port = process.env.PORT || 5000;
